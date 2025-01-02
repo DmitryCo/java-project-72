bf670b2dececf5c3f98e080003e03b4225b85dc3
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-02 19:44:36 +0300

    Add getApp(), getPort()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,2 +1,22 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(javalinConfig -> {
+            javalinConfig.bundledPlugins.enableDevLogging();
+        }).get("/", ctx -> ctx.result("Hello World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(getPort());
+    }
 }
