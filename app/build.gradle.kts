784ca4f85a07ec027f39b8d1e58cc798c155be6c
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-02 19:43:47 +0300

    Add shadow plugin, javalin, slf4j

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("application")
     id("checkstyle")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 application {
@@ -18,6 +19,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
+    implementation("io.javalin:javalin-bundle:6.3.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
