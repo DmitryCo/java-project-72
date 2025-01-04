914be22ced221822f67cfb4643d110b41ac2035b
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-04 23:53:35 +0300

    Add controllers

--- app/src/main/java/hexlet/code/controller/MainPageController.java
+++ app/src/main/java/hexlet/code/controller/MainPageController.java
@@ -1,2 +1,15 @@
-package hexlet.code.controller;public class MainPageController {
+package hexlet.code.controller;
+
+import hexlet.code.dto.MainPage;
+
+import io.javalin.http.Context;
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+public class MainPageController {
+    public static void welcomeMain(Context ctx) {
+        MainPage page = new MainPage();
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("index.jte", model("page", page));
+    }
 }
