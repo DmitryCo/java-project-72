7d6d9429c31d25300898dfc40a14551c62c31665
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-05 15:10:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
-    id("java")
     id("application")
     id("checkstyle")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("jacoco")
     id("io.freefair.lombok") version "8.6"
-}
-
-application {
-    mainClass.set("hexlet.code.App")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "hexlet.code"
@@ -18,19 +15,38 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("io.javalin:javalin:6.3.0")
-    implementation("io.javalin:javalin-rendering:6.3.0")
-    implementation("io.javalin:javalin-bundle:6.3.0")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
-    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.h2database:h2:2.3.230")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.postgresql:postgresql:42.7.3")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("org.webjars:bootstrap:5.3.3")
+    implementation("io.javalin:javalin:6.2.0")
+    implementation("io.javalin:javalin-bundle:6.2.0")
+    implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("gg.jte:jte:3.1.9")
     implementation("com.konghq:unirest-java:3.13.0")
     implementation ("org.jsoup:jsoup:1.18.1")
 }
 
+application {
+    mainClass.set("hexlet.code.App")
+}
+
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
