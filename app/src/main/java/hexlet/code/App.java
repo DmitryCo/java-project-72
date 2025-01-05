95c5f764a73bcd982c5f7760aaa4c26f6176c9b4
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-05 14:31:06 +0300

    Update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,48 +1,78 @@
 package hexlet.code;
 
+import hexlet.code.controller.MainPageController;
+import hexlet.code.controller.UrlPageController;
+import hexlet.code.controller.UrlsController;
 import hexlet.code.repository.BaseRepository;
+import hexlet.code.util.Routes;
 
-import io.javalin.Javalin;
-import io.javalin.rendering.template.JavalinJte;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.io.IOException;
-import java.sql.SQLException;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
+import io.javalin.Javalin;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App {
     private static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System.getenv().getOrDefault(
+                "PORT",
+                "7070");
         return Integer.parseInt(port);
     }
 
     private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+        return System.getenv().getOrDefault(
+                "JDBC_DATABASE_URL",
+                "jdbc:h2:mem:project");
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 
     private static TemplateEngine createTemplateEngine() {
         ClassLoader classLoader = App.class.getClassLoader();
-        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
-        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
-        return templateEngine;
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver(
+                "templates", classLoader);
+        return TemplateEngine.create(codeResolver, ContentType.Html);
     }
 
     public static Javalin getApp() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(getDatabaseUrl());
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        String sql = readResourceFile("schema.sql");
+        log.info(sql);
 
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
+            statement.execute(sql);
         }
         BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(javalinConfig -> {
             javalinConfig.bundledPlugins.enableDevLogging();
             javalinConfig.fileRenderer(new JavalinJte(createTemplateEngine()));
-        }).get("/", ctx -> ctx.result("Hello World"));
+        });
+        app.get(Routes.mainPath(), MainPageController::welcomeMain);
+        app.post(Routes.urlsPath(), UrlsController::createUrl);
+        app.get(Routes.urlsPath(), UrlsController::showUrls);
+        app.get(Routes.urlPath("{id}"), UrlPageController::showUrlPage);
+        app.post(Routes.urlChecks("{id}"), UrlPageController::urlCheck);
         return app;
     }
 
