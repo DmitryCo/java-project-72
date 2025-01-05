29dc1396c8c339d1d4013d55b4dfc4893a028488
Dmitry Konstantinov <dm3ypost@gmail.com>
2025-01-05 14:28:40 +0300

    Add /model/Url and /model/UrlCheck

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -1,2 +1,27 @@
-package hexlet.code.model;public class UrlCheck {
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class UrlCheck {
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private LocalDateTime createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+    }
 }
